029ee502a5c5ae2a24dee5e18c758718dfabd299
Zimo Yang <zimoyang@andrew.cmu.edu>
2022-03-04 15:30:28 -0500

    update: design the login page(version 1)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
 
 const Login = () =>  {
 	const userRef = useRef();
@@ -17,8 +21,17 @@ const Login = () =>  {
 		 component = "form"
 		 noValidate
 		 autoComplete="off"
+		 sx={{
+            marginTop: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+			mt: 1
+          }}
 		 >
 				<TextField
+					sx={{ mt: 1, mb: 1, width: '11%'}}
+					autoFocus
 					ref={userRef}
 					onChange={(e) => setUser(e.target.value)}
 					value={user}
@@ -27,13 +40,37 @@ const Login = () =>  {
 					id="outlined-required"
 				/>
 					<TextField
+					margin="normal"
+					required
+					sx={{ mt: 1, mb: 1, width: '11%'}}
 					onChange={(e) => setPassword(e.target.value)}
 					value={password}
 					id="outlined-password-input"
-          label="Password"
-          type="password"
+					label="Password"
+					type="password"
 				/>
-				<Button variant="contained">Sign in</Button>
+				<Grid container sx={{ mr: 10, width: '11%'}} >
+				<Grid item xs>
+					<Link href="#" variant="body2">
+					Forgot password?
+					</Link>
+				</Grid>
+				</Grid>	
+				<FormControlLabel
+				    sx={{ mr: 1, width: '11%'}}
+					control={<Checkbox value="remember" color="primary" />}
+					label="Remember me"
+				/>
+				<Button 
+				variant="contained" 
+				sx={{ mt: 1, mb: 2, width: '11%'}}
+				style={{
+                backgroundColor: "green",
+                color: "white"
+              	}}>
+					Sign in
+					</Button>
+
 			</Box>
   )
 }
