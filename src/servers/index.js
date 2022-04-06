c5a7c71f08e6ed6aeffdb68a4be09277e19b816e
Zimo Yang <zimoyang@andrew.cmu.edu>
2022-04-06 01:39:45 -0400

    add mvc for database connection and test post interface

---
+++ src/servers/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import db from "./config/db.js";
+import OppRoutes from "./routes/index.js";
+import cors from "cors";
+
+const app = express();
+
+try {
+    await db.authenticate();
+    console.log('Database connected...');
+} catch (error) {
+    console.error('Connection error:', error);
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/opportunities', OppRoutes);
+
+app.listen(5001, () => console.log('Server running at port 5001'));
---
+++ src/servers/models/opportunitiesModel.js
@@ -0,0 +1,57 @@
+import { Sequelize } from "sequelize";
+import db from "../config/db.js";
+
+const { DataTypes } = Sequelize;
+
+const Opportunity = db.define('opportunities',{
+    name:{
+        type: DataTypes.STRING
+    },
+    category:{
+        type: DataTypes.STRING
+    },
+    description:{
+        type: DataTypes.STRING
+    },
+    qualifications:{
+        type: DataTypes.STRING
+    },
+    modality:{
+        type: DataTypes.STRING
+    },
+    paid:{
+        type: DataTypes.BOOLEAN
+    },
+    cost:{
+        type: DataTypes.INTEGER
+    },
+    stipend:{
+        type: DataTypes.INTEGER
+    },
+    financial_aid:{
+        type: DataTypes.BOOLEAN
+    },
+    website:{
+        type: DataTypes.STRING
+    },
+    image_path:{
+        type: DataTypes.STRING
+    },
+    reviews:{
+        type: DataTypes.STRING
+    },
+    start_date:{
+        type: DataTypes.DATE
+    },
+    end_date:{
+        type: DataTypes.DATE
+    },
+    application_deadline:{
+        type: DataTypes.DATE
+    }
+},{
+    freezeTableName: true, // Model tableName will be the same as the model name
+    timestamps: false
+});
+
+export default Opportunity;
